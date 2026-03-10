2ededd256c534dc4749bd195d68f08a458bbc739
Akash Kumar Keshri <40911084+Akash9801@users.noreply.github.com>
2026-03-10 16:08:45 +0530

    Add files via upload

---
+++ program1.py
@@ -0,0 +1,14 @@
+def reflex_vacuum_agent(location, status):
+    if status == "Dirty":
+        return "Suck"
+    elif location == "A":
+        return "Right"
+    elif location == "B":
+        return "Left"
+    
+location ="A"
+status = "Dirty"
+
+action = reflex_vacuum_agent(location, status)
+print(f"location: {location}, Status: {status}, Action: {action}")
+    
